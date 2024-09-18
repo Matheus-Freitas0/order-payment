e4c59ee12a24e3206575aa85d30164c95e4f48c3
MatheusFreitas <moncaomatheus@gmail.com>
2024-09-18 09:59:22 -0300

    Added new variables to OrderPaymentController class

--- src/controller/order-payment.controller.ts
+++ src/controller/order-payment.controller.ts
@@ -13,3 +13,21 @@ export class OrderPaymentController {
     }
 
 }
+
+    // dados para serem retornado
+    // nome do cliente
+    // valor da compra
+    // data da compra
+    // documento 
+    // 4 ultimos numeros do cartão 
+    // data de validade do cartão
+    // tipo de cartão
+    // codigo do cartão
+    // ID da transação
+    // status da transação
+    // Método de Pagamento
+    // Nome da loja 
+    // numero do pedido
+    // descrição da compra
+    // endereço de entrega
+    // taxas
