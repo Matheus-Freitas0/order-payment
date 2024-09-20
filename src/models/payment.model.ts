c75c80fbabd34bc097ac263e75a21374174ad637
Matheus-Freitas0 <moncaomatheus@gmail.com>
2024-09-20 14:21:13 -0300

    payment model has been started

---
+++ src/models/payment.model.ts
@@ -0,0 +1,16 @@
+import mongoose from 'mongoose'
+
+const paymentSchema = new mongoose.Schema({
+    
+    // - amount
+    // - created
+    // - cardNumber (4 last)
+    // - transactionNumber
+    // - partnerId
+    // - paymentMethod
+    // - clientDocument
+    // - orderCode
+
+})
+
+export default mongoose.model('Payment', paymentSchema)
