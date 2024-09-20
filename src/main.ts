1b8b284dee95667d95c66918d16af2a5aad42c43
Matheus-Freitas0 <moncaomatheus@gmail.com>
2024-09-20 14:19:37 -0300

    Added mongooseConnect import and call, updated ExpressServerConfig import path, and reformatted code

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,10 @@
-import { ExpressServerConfig } from './express-server.config'
+import { mongooseConnect } from './config/mongo.config'
+import { ExpressServerConfig } from './config/express-server.config'
 
-
+mongooseConnect()
 const expressServer = new ExpressServerConfig()
-expressServer
-.basicConfig()
-.routesRegistry()
-.startServer()
 
+expressServer
+    .basicConfig()
+    .routesRegistry()
+    .startServer()
