1ece662437ac59904ac945305d2664bd08d063bd
MatheusFreitas <moncaomatheus@gmail.com>
2024-10-01 13:23:41 -0300

    Added payment strategy pattern implementation to PaymentService

--- src/service/payment.service.ts
+++ src/service/payment.service.ts
@@ -1,6 +1,11 @@
 import { PaymentRepository } from "../repository/payment.repository"
 import { sleep } from "../utils/utils"
 import {PaymentDTO} from "../DTO/payment.dto"
+import { PaymentExecutor } from '../strategy/payment-executor'
+import { PayPix } from '../strategy/methods/pay-pix'
+import { PayDebit } from '../strategy/methods/pay-debit'
+import { PayCash } from '../strategy/methods/pay-cash'
+import { PayCredit } from '../strategy/methods/pay-credit'
 
 export class PaymentService{
 
@@ -12,7 +17,33 @@ export class PaymentService{
 
     async executePaymentTransaction(paymentInstrument: PaymentDTO): Promise<void>{
         await sleep(5000)
-        
+        let paymentExecutor: PaymentExecutor
+
+        switch (paymentInstrument.method) {
+            case "PIX":
+                paymentExecutor = new PayPix()
+                break;
+            case "CREDIT":
+                paymentExecutor = new PayCredit()
+                break;
+            case "DEBIT":
+                paymentExecutor = new PayDebit()
+                break;
+            case "CASH":
+                paymentExecutor = new PayCash()
+                break;
+            
+            default:
+                throw new Error(`Invalid payment method or not implemented yet. method =${paymentInstrument.method}`)
+        }
+
+        try {
+            await paymentExecutor.execute(paymentInstrument)
+            await this.paymentRepository.create(paymentInstrument, 'SUCCESS', 'success')
+        } catch(error: any) {
+            await this.paymentRepository.create(paymentInstrument, 'FAILED', error.message)
+        }
+       
     }
 
     async getPaymentByOrderCode(orderCode: string): Promise<any>{
